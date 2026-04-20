3f42f288f9fe3313bea7b82cd5a5f50a03d0420c
Yağmur Gökçekli <yagmurgokcekli@gmail.com>
2026-04-20 11:05:20 +0300

    feat: save current

--- backend/app/logic/receipt_logic.py
+++ backend/app/logic/receipt_logic.py
@@ -1,18 +1,17 @@
 from fastapi import UploadFile
 from ..services.BlobStorageService import blob_storage_service
-from ..schemas.receipt_schema import Engine
+from ..schemas.receipt_schema import Engine, Receipt
 
 
-async def process_receipt(engine: Engine, file: UploadFile):
-
+async def process_receipt(engine: Engine, file: UploadFile) -> Receipt:
     blob = await save_to_blob(file)
 
-    return {
-        "filename": file.filename,
-        "content_type": file.content_type,
-        "blob": blob,
-        "engine": engine,
-    }
+    return Receipt(
+        filename=file.filename,
+        content_type=file.content_type,
+        sas_url=blob["sas_url"],
+        engine=engine,
+    )
 
 
 async def save_to_blob(file: UploadFile):
